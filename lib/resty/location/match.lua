760553a3f53df2422429d7e64207e9ac896a3eb9
张策 <zhangce5413@gmail.com>
2016-12-21 22:37:43 +0800

    add version number

--- lib/resty/location/match.lua
+++ lib/resty/location/match.lua
@@ -2,7 +2,7 @@ local string_len = string.len
 local re_match = ngx.re.match
 
 local _M = {
-    _VERSION = '0.3.0'
+    _VERSION = '0.3.1'
 }
 
 function _M:match_location(uri, location_match_list)
